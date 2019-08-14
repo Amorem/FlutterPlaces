e1928701bf11e18e2ad674fb3e76385f23089e0d
AMOREM <amorem@amorem.com>
2019-08-14 14:46:08 +0200

    Location package + Authorizations

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,9 +1,11 @@
-import 'package:flutter/material.dart';
 import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/places.dart';
 import '../widgets/image_input.dart';
+import '../widgets/location_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -51,6 +53,10 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       height: 10,
                     ),
                     ImageInput(_selectImage),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    LocationInput()
                   ],
                 ),
               ),
